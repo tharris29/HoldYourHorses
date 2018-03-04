57cfd0d8a63dd7b9da11418477f5d5e684b09658
Thomas Harris <thomas.harris.2916@gmail.com>
2018-03-04 17:58:39 +0000

    initial dump of working files from main project

---
+++ HoldYourHorses/ThrottleAttribute.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Caching;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+namespace HoldYourHorses.ActionFilters
+{
+    public class ThrottleAttribute : ActionFilterAttribute
+    {
+        public int RateLimitSeconds { get; set; }
+        public int CallsPerRateLimitSeconds { get; set; }
+
+        public override void OnActionExecuting(HttpActionContext actionContext)
+        {
+            actionContext.Request.Headers.TryGetValues("Authorization", out var keys);
+
+            var key = keys.FirstOrDefault();
+            if (key == null) return;
+
+            if (IsRateLimited(key))
+            {
+                SetRateLimitExceededMessage(actionContext);
+            }
+        }
+
+        private void SetRateLimitExceededMessage(HttpActionContext actionContext)
+        {
+            actionContext.Response = new HttpResponseMessage
+            {
+                StatusCode = (HttpStatusCode)429,
+                ReasonPhrase = "Too Many Requests",
+                Content = new StringContent($"Rate limit reached {CallsPerRateLimitSeconds} calls in {RateLimitSeconds} seconds.")
+            };
+        }
+
+        private bool IsRateLimited(string key)
+        {
+
+            var callRateForAuthorizationCode = (CallRateForAuthorizationCode)HttpRuntime.Cache.Get(key);
+
+            SetDefaultRateLimit();
+
+            if (callRateForAuthorizationCode != null)
+            {
+
+                if (TooManyCallsInRateLimitTime(callRateForAuthorizationCode)) return true;
+
+                callRateForAuthorizationCode.Calls++;
+                SetCacheValue(key, callRateForAuthorizationCode);
+            }
+            else
+            {
+                SetCacheValue(key, new CallRateForAuthorizationCode { Calls = 1, FirstCallTime = DateTime.Now });
+            }
+
+            return false;
+        }
+
+        private bool TooManyCallsInRateLimitTime(CallRateForAuthorizationCode callRateForAuthorizationCode)
+        {
+            var secondsPassedSinceFirstRecordedCall = DateTime.Now.Subtract(callRateForAuthorizationCode.FirstCallTime).Seconds;
+            return callRateForAuthorizationCode.Calls >= CallsPerRateLimitSeconds && secondsPassedSinceFirstRecordedCall <= RateLimitSeconds;
+        }
+
+        private void SetDefaultRateLimit()
+        {
+            if (RateLimitSeconds == 0)
+            {
+                RateLimitSeconds = 30;
+            }
+
+            if (CallsPerRateLimitSeconds == 0)
+            {
+                CallsPerRateLimitSeconds = 10;
+            }
+        }
+
+
+        private void SetCacheValue(string key, CallRateForAuthorizationCode callRateForAuthorizationCode)
+        {
+            HttpRuntime.Cache.Insert(key,
+                callRateForAuthorizationCode,
+                null,
+                DateTime.Now.AddSeconds(RateLimitSeconds * 2),
+                Cache.NoSlidingExpiration,
+                CacheItemPriority.Low,
+                null);
+        }
+    }
+
+    public class CallRateForAuthorizationCode
+    {
+        public int Calls { get; set; }
+
+        public DateTime FirstCallTime { get; set; }
+    }
+}
---
+++ HoldYourHorsesTest/ThrottleAttributeTest.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Net.Http;
+using System.Web.Http.Controllers;
+using NUnit.Framework;
+
+namespace HoldYourHorses.Test
+{
+    [TestFixture]
+    public class ThrottleAttributeTest
+    {
+        private HttpActionContext CreateHttpContext(string key)
+        {
+            var context = new HttpActionContext();
+            var controllerContext = new HttpControllerContext { Request = new HttpRequestMessage() };
+            controllerContext.Request.Headers.Add("Authorization", key);
+            context.ControllerContext = controllerContext;
+
+            return context;
+        }
+
+        [Test]
+        public void CallsExceedAllowedCallsInTimeframe()
+        {
+            var throttle = new ThrottleAttribute() { CallsPerRateLimitSeconds = 1, RateLimitSeconds = 1 };
+            var authCode = "Key1" + Guid.NewGuid();
+            var context = CreateHttpContext(authCode);
+            throttle.OnActionExecuting(context);
+            System.Threading.Thread.Sleep(TimeSpan.FromSeconds(1));
+
+            var context2 = CreateHttpContext(authCode);
+            throttle.OnActionExecuting(context2);
+
+            Assert.AreEqual(context2.Response.StatusCode.ToString(), 429.ToString());
+        }
+
+        [Test]
+        public void MultipleCallsFromDifferentUsersDoNotExceed()
+        {
+            var throttle = new ThrottleAttribute() { CallsPerRateLimitSeconds = 1, RateLimitSeconds = 1 };
+
+            var context = CreateHttpContext("Key3" + Guid.NewGuid());
+            throttle.OnActionExecuting(context);
+
+            System.Threading.Thread.Sleep(TimeSpan.FromSeconds(1));
+
+            var context2 = CreateHttpContext("Key4" + Guid.NewGuid());
+            throttle.OnActionExecuting(context2);
+
+            Assert.AreEqual(context2.Response, null);
+        }
+
+        [Test]
+        public void CallsDoNotExceedLimit()
+        {
+            var throttle = new ThrottleAttribute { CallsPerRateLimitSeconds = 3, RateLimitSeconds = 1 };
+            var authCode = "Key2" + Guid.NewGuid();
+            var context = CreateHttpContext(authCode);
+            throttle.OnActionExecuting(context);
+
+            var context2 = CreateHttpContext(authCode);
+            throttle.OnActionExecuting(context2);
+
+            Assert.AreEqual(context2.Response, null);
+        }
+
+        [Test]
+        public void DefaultsAreSetForThrottling()
+        {
+            var throttle = new ThrottleAttribute();
+
+            var context = CreateHttpContext("Key" + Guid.NewGuid());
+            throttle.OnActionExecuting(context);
+            throttle.OnActionExecuting(context);
+
+            Assert.AreEqual(throttle.CallsPerRateLimitSeconds, 10);
+            Assert.AreEqual(throttle.RateLimitSeconds, 30);
+        }
+
+        [Test]
+        public void SuccessfullyMakeCallsAfterThrottlingEnds()
+        {
+            var throttle = new ThrottleAttribute() { CallsPerRateLimitSeconds = 1, RateLimitSeconds = 1 };
+            var authCode = "Key8" + Guid.NewGuid();
+            var context = CreateHttpContext(authCode);
+            throttle.OnActionExecuting(context);
+            System.Threading.Thread.Sleep(TimeSpan.FromSeconds(1));
+
+            var context2 = CreateHttpContext(authCode);
+            throttle.OnActionExecuting(context2);
+
+            Assert.AreEqual(context2.Response.StatusCode.ToString(), 429.ToString());
+
+
+            System.Threading.Thread.Sleep(TimeSpan.FromSeconds(2));
+
+            var context3 = CreateHttpContext(authCode);
+            throttle.OnActionExecuting(context3);
+
+            Assert.AreEqual(context3.Response, null);
+        }
+    }
+}
